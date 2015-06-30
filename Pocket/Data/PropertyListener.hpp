466b590ac494b8963230716f7cceb83c14e86896
JeppeNielsen <nielsen_jeppe@hotmail.com>
2015-06-30 20:12:01 +0200

    added propertyListener

---
+++ Pocket/Data/PropertyListener.hpp
@@ -0,0 +1,36 @@
+//
+//  PropertyListener.h
+//  Homeland
+//
+//  Created by Jeppe Nielsen on 15/06/15.
+//  Copyright (c) 2015 Jeppe Nielsen. All rights reserved.
+//
+
+#pragma once
+#include "Property.hpp"
+#include <set>
+
+namespace Pocket {
+    template<class Context>
+    class PropertyListener {
+		public:
+            template<class Owner, class Value>
+            void Add(Property<Owner, Value>& property, Context context) {
+            	property.Changed += event_handler(this, &PropertyListener::PropertyChanged<Owner>, context);
+                changedObjects.insert(context);
+            }
+            template<class Owner, class T>
+        	void Remove(Property<Owner, T>& property, Context object) {
+            	property.Changed -= event_handler(this, &PropertyListener::PropertyChanged<Owner>, object);
+            }
+        	typedef std::set<Context> ChangedObjects;
+            const ChangedObjects& Objects() { return changedObjects; }
+        	void Clear() { changedObjects.clear(); }
+        private:
+        	template<class Owner>
+        	void PropertyChanged(Owner owner, Context context) {
+            	changedObjects.insert(context);
+            }
+			ChangedObjects changedObjects;
+    };
+}
