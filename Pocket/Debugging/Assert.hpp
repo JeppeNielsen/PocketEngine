b4665c991b72a7d9cc6e8f8ddb66fd5f5199c97e
JeppeNielsen <nielsen_jeppe@hotmail.com>
2015-09-17 21:11:02 +0200

    Assert changed

--- Pocket/Debugging/Assert.hpp
+++ Pocket/Debugging/Assert.hpp
@@ -1,14 +1,5 @@
 #pragma once
-#include <iostream>
 #include <assert.h>
 
-
-#define ASSERT(condition, message) do { } while (false);
-
-/*
-std::cerr << "Assertion `" #condition "` failed in " << __FUNCTION__ \
-                      << " line " << __LINE__ << ": " << message << std::endl; \
-
-					   std::exit(EXIT_FAILURE); \
-*/
+#define ASSERT(condition, message) assert(condition)
 
