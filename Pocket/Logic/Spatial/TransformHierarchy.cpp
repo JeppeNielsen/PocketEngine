64992e3c5d38c819b8f507321e59f2353d570af4
JeppeNielsen <nielsen_jeppe@hotmail.com>
2016-10-03 19:25:11 +0200

    World space position/rotation and scaled kept when reparenting.

--- Pocket/Logic/Spatial/TransformHierarchy.cpp
+++ Pocket/Logic/Spatial/TransformHierarchy.cpp
@@ -38,17 +38,74 @@ void TransformHierarchy::ObjectRemoved(GameObject* object) {
 
 void TransformHierarchy::ParentChanged(GameObject* object) {
     Transform* transform = object->GetComponent<Transform>();
+    
+    /*
+    Matrix4x4 prevWorld = transform->World;
+    Vector3 scaleX = prevWorld.TransformVector({1,0,0});
+    Vector3 scaleY = prevWorld.TransformVector({0,1,0});
+    Vector3 scaleZ = prevWorld.TransformVector({0,0,1});
+    */
+    
+    const Matrix4x4 oldWorld = transform->World;
+    
     transform->World.MakeDirty();
     transform->WorldInverse.MakeDirty();
+    
+    Transform* oldParentTransform = object->Parent().PreviousValue() ?
+    object->Parent().PreviousValue()->GetComponent<Transform>()
+    :
+    0;
 
-    if (object->Parent().PreviousValue()) {
-        Transform* parentTransform = object->Parent().PreviousValue()->GetComponent<Transform>();
-        if (parentTransform) {
-            transform->UnHookOther(parentTransform);
-        }
+    if (oldParentTransform) {
+        transform->UnHookOther(oldParentTransform);
     }
     
-    HookParent(transform, object->Parent());
+    Transform* newParentTransform = object->Parent() ?
+    object->Parent()()->GetComponent<Transform>()
+    :
+    0;
+    
+    if (newParentTransform) {
+        transform->HookOther(newParentTransform);
+    }
+
+    const Matrix4x4& newWorld = !newParentTransform ?
+        Matrix4x4::IDENTITY
+        :
+        newParentTransform->WorldInverse;
+
+    Matrix4x4 intoNewWorld = newWorld * oldWorld;
+    
+    transform->Position = intoNewWorld.TransformPosition(0);
+    transform->Scale = {
+        intoNewWorld.TransformVector({1,0,0}).Length(),
+        intoNewWorld.TransformVector({0,1,0}).Length(),
+        intoNewWorld.TransformVector({0,0,1}).Length()
+    };
+    
+
+    Vector3 x = { intoNewWorld.m[0][0], intoNewWorld.m[1][0], intoNewWorld.m[2][0] };
+    Vector3 y = { intoNewWorld.m[0][1], intoNewWorld.m[1][1], intoNewWorld.m[2][1] };
+    Vector3 z = { intoNewWorld.m[0][2], intoNewWorld.m[1][2], intoNewWorld.m[2][2] };
+    x.Normalize();
+    y.Normalize();
+    z.Normalize();
+    
+    intoNewWorld.m[0][0] = x.x;
+    intoNewWorld.m[1][0] = x.y;
+    intoNewWorld.m[2][0] = x.z;
+    
+    intoNewWorld.m[0][1] = y.x;
+    intoNewWorld.m[1][1] = y.y;
+    intoNewWorld.m[2][1] = y.z;
+    
+    intoNewWorld.m[0][2] = z.x;
+    intoNewWorld.m[1][2] = z.y;
+    intoNewWorld.m[2][2] = z.z;
+    
+    Quaternion quat;
+    quat.FromRotationMatrix(intoNewWorld);
+    transform->Rotation = quat;
 }
 
 void TransformHierarchy::HookParent(Transform* transform, GameObject* parent) {
