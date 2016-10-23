57c8ad638582cd9619055f01c8d6432803424d0a
JeppeNielsen <nielsen_jeppe@hotmail.com>
2016-10-23 13:35:49 +0200

    Added Extra logic tests

--- Projects/TestComponentSystem/TestComponentSystem/Tests/LogicTests.cpp
+++ Projects/TestComponentSystem/TestComponentSystem/Tests/LogicTests.cpp
@@ -8,7 +8,66 @@
 
 #include "LogicTests.hpp"
 #include <cstdlib>
+#include "GameWorld.hpp"
+#include "GameSystem.hpp"
+
+using namespace Pocket;
+
 
 void LogicTests::RunTests() {
+    
+    AddTest("Create World", [] {
+        GameWorld world;
+        return world.Roots().size() == 0;
+    });
+
+    AddTest("Create Root", [] {
+        GameWorld world;
+        GameObject* root = world.CreateRoot();
+        return root && world.Roots().size() == 1;
+    });
+    
+    AddTest("Delete root", [] {
+        GameWorld world;
+        GameObject* root = world.CreateRoot();
+        bool wasOne = world.Roots().size() == 1;
+        root->Remove();
+        world.Update(0);
+        return wasOne && world.Roots().size() == 0;
+    });
+    
+    AddTest("Create Child", [] {
+        GameWorld world;
+        GameObject* root = world.CreateRoot();
+        GameObject* child = root->CreateChild();
+        return root && child && root->Children().size() == 1;
+    });
+
+    AddTest("Delete Child", [] {
+        GameWorld world;
+        GameObject* root = world.CreateRoot();
+        GameObject* child = root->CreateChild();
+        bool wasOne = root->Children().size() == 1;
+        child->Remove();
+        world.Update(0);
+        return wasOne && root->Children().size() == 0;
+    });
+
+    AddTest("Object Handle operator->, ", [] {
+        GameWorld world;
+        GameObject* root = world.CreateRoot();
+        Handle<GameObject> handle = root;
+        return handle.operator->() == root;
+    });
+
+    AddTest("Handle invalid on removed object ", [] {
+        GameWorld world;
+        GameObject* root = world.CreateRoot();
+        Handle<GameObject> handle = root;
+        bool wasValid = handle ? true : false;
+        root->Remove();
+        world.Update(0);
+        return wasValid && !handle;
+    });
 
 }
