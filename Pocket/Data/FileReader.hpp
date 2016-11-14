32fd81e2048dae28fc68f0e48ba1447ab03f778e
JeppeNielsen <nielsen_jeppe@hotmail.com>
2016-11-14 20:38:14 +0100

    Added recursive find files to FileReader

--- Pocket/Data/FileReader.hpp
+++ Pocket/Data/FileReader.hpp
@@ -23,6 +23,8 @@ namespace Pocket {
         static std::string ShowSaveFileRequester(const std::string& path);
         
         static void OpenPathInFileExplorer(const std::string& path);
-        
+        static void FindFiles(std::vector<std::string>& list, const std::string& path, const std::string& extension);
+    private:
+        static void FindFilesAtPath(std::vector<std::string>& list, const std::string& path, const std::string& extension);
     };
 }
