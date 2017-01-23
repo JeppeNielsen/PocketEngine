e2373b8965678eafd85b4a508a5af819d428db7a
JeppeNielsen <nielsen_jeppe@hotmail.com>
2017-01-23 22:08:23 +0100

    removed shader and texture code when creating new gameobject from menu. Ignoring cloner objects when duplicating objects.

--- Editor/PocketEditor/PocketEditor/Logic/Menus/GameObjectMenu/GameObjectMenu.cpp
+++ Editor/PocketEditor/PocketEditor/Logic/Menus/GameObjectMenu/GameObjectMenu.cpp
@@ -9,6 +9,7 @@
 #include "GameObjectMenu.hpp"
 #include "Timer.hpp"
 #include "ShaderComponent.hpp"
+#include "Cloner.hpp"
 
 std::string GameObjectMenu::Name() {
     return "GameObject";
@@ -26,43 +27,6 @@ void GameObjectMenu::OnCreate() {
         object->AddComponent<Mesh>()->GetMesh<Vertex>().AddCube(0, 1);
         object->AddComponent<Renderable>();
         object->AddComponent<EditorObject>();
-        
-        auto& shader = object->AddComponent<ShaderComponent>()->GetShader<Vertex>();
-        
-        shader.Load(
-            "attribute vec4 Position;                   "
-            "attribute vec2 TexCoords;                  "
-            "attribute vec4 Color;                      "
-            "attribute vec3 Normal;                     "
-            "uniform mat4 ViewProjection;               "
-            "varying vec2 vTexCoords;                   "
-            "varying vec4 vColor;                       "
-            "varying vec3 vNormal;                      "
-            "void main() {                              "
-            "	vTexCoords = TexCoords;                 "
-            "   vColor = Color;                         "
-            "   vNormal = Normal;                       "
-            "	gl_Position = Position * ViewProjection;"
-            "}                                          "
-            ,
-            "varying vec2 vTexCoords;                   "
-            "varying vec4 vColor;                       "
-            "varying vec3 vNormal;                      "
-            "uniform vec3 LightDirection;               "
-            "uniform vec4 AmbientLight;                 "
-            "uniform sampler2D Texture;                 "
-            "void main() {                              "
-            "   float n = clamp(dot(LightDirection, vNormal),0.0,1.0);   "
-            "   vec4 textureColor = texture2D(Texture, vTexCoords);      "
-            "   vec4 color =  textureColor * (AmbientLight + vColor * n);             "
-            "	gl_FragColor = vec4(color.r, color.g, color.b, textureColor.a * vColor.a);"
-            "}"
-        );
-        shader.SetValue("LightDirection", Vector3(1,1,1).Normalized());
-        shader.SetValue("AmbientLight", Colour(0.2f));
-        
-        object->AddComponent<TextureComponent>()->Texture().LoadFromPng("/Projects/PocketEngine/Editor/Pong//Gfx/pipe.png");
-        
     });
     menu->AddChild("Delete", "D").Clicked.Bind([this] {
         selectables = context->Project().GetSelectables();
@@ -75,7 +39,12 @@ void GameObjectMenu::OnCreate() {
         for(auto o : selectables->Selected()) {
             GameObject* source = o->GetComponent<EditorObject>()->gameObject;
             if (source->IsRoot()) continue;
-            source->CreateCopy();
+            source->CreateCopy([] (GameObject* object) {
+                if (object->Parent() && object->Parent()->GetComponent<Cloner>()) {
+                    return false;
+                }
+                return true;
+            });
          }
     });
 }
