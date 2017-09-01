a84309d4742f24b8b7651dc29f71cef838e09cc2
JeppeNielsen <nielsen_jeppe@hotmail.com>
2017-09-01 17:50:52 +0200

    RenderSize renamed to RenderTextureSize

--- Editor/PocketEditor/PocketEditor/Logic/Windows/WorldTab/WorldTab.cpp
+++ Editor/PocketEditor/PocketEditor/Logic/Windows/WorldTab/WorldTab.cpp
@@ -26,7 +26,7 @@ void WorldTab::OnCreate() {
 
     renderArea = context->Gui().CreatePivot(window);
     renderArea->AddComponent<GameWorldViewport>()->World = &context->World();
-    renderArea->GetComponent<GameWorldViewport>()->RenderSize = {2880, 1800};
+    renderArea->GetComponent<GameWorldViewport>()->RenderTextureSize = {2880, 1800};
     
     renderArea->AddComponent<Touchable>();
     renderArea->AddComponent<TextureComponent>();
