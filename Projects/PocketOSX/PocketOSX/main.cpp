fde60a151cfed43ab7d8b010cb9d1f6c7ca7e5d5
JeppeNielsen <nielsen_jeppe@hotmail.com>
2017-12-29 21:30:34 +0100

    PocketOSX commited

--- Projects/PocketOSX/PocketOSX/main.cpp
+++ Projects/PocketOSX/PocketOSX/main.cpp
@@ -19,7 +19,7 @@ using namespace Pocket;
 class Game : public GameState<Game> {
 public:
     GameWorld world;
-    GameObject* camera;
+    GameObject* root;
     GameObject* cube;
     float rotation;
     
@@ -47,56 +47,33 @@ public:
     };
     
     void Initialize() {
+    
+        root = world.CreateRoot();
         
-        world.CreateSystem<RenderSystem>();
-        world.CreateSystem<TouchSystem>()->Input = &Input;
-        world.CreateSystem<DraggableSystem>();
-        world.CreateSystem<ClickColorSystem>();
-        world.CreateSystem<FirstPersonMoverSystem>()->Input = &Input;
         
-        camera = world.CreateObject();
+        root->CreateSystem<RenderSystem>();
+        root->CreateSystem<TouchSystem>();
+        root->CreateSystem<DraggableSystem>();
+        root->CreateSystem<ClickColorSystem>();
+        root->CreateSystem<FirstPersonMoverSystem>();
+        
+        auto camera = root->CreateObject();
         camera->AddComponent<Camera>();
         camera->AddComponent<Transform>()->Position = { 0, 0, 10 };
         camera->GetComponent<Camera>()->FieldOfView = 40;
         camera->AddComponent<FirstPersonMover>();
         
-        /*
-        
-        cube = world.CreateObject();
+        auto cube = root->CreateObject();
         cube->AddComponent<Transform>();
         cube->AddComponent<Mesh>()->GetMesh<Vertex>().AddCube(0, 1);
-        cube->AddComponent<Material>();
-        cube->AddComponent<Draggable>();
+        cube->AddComponent<Renderable>();
         cube->AddComponent<Touchable>();
         
-        std::ofstream file;
-        file.open("mesh.json");
-        cube->ToJson(file);
-        file.close();
-        */
         
-        std::ifstream file;
-        file.open("mesh.json");
-        cube = world.CreateObject(file);
-        file.close();
-        
-        
-        auto& verts = cube->GetComponent<Mesh>()->GetMesh<Vertex>().vertices;
-        
-        for (int i=0; i<verts.size(); i++) {
-            verts[i].Color = Colour::HslToRgb(i * 10, 1, 1, 1);
-        }
-        
-        rotation = 0;
-    
-        
-    
-    
     }
     
     void Update(float dt) {
-        cube->GetComponent<Transform>()->Rotation = Quaternion(rotation, Vector3(1,0.2f,0.5f).Normalized());
-        rotation += dt;
+        Context().InputDevice().UpdateInputManager(&world.Input());
         world.Update(dt);
     }
     
--- Projects/PocketOSX/PocketOSX/testGui.cpp
+++ Projects/PocketOSX/PocketOSX/testGui.cpp
@@ -1,5 +1,4 @@
 #include "Engine.hpp"
-#include "Gui.hpp"
 #include <fstream>
 #include "Timer.hpp"
 #include <sstream>
