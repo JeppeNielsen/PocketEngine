1ac9347c6b12b9f12f462b69ea538834734aa8c9
JeppeNielsen <nielsen_jeppe@hotmail.com>
2017-12-29 21:25:28 +0100

    removed old editorproject

--- EditorProjects/Pong/Code/Rotator.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "Rotator.hpp"
-#include "Box.hpp"
-
-void RotatorSystem::Update(float dt) {
-    for(auto o : Objects()) {
-        o->GetComponent<Transform>()->EulerRotation += o->GetComponent<Rotator>()->speed * dt;
-    }
-}
